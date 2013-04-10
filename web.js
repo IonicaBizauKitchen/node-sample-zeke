f1fc83c66bcb21cdca7e0b5746b3df0c5579c210
zeke <zeke@sikelianos.com>
2013-04-10 15:40:15 -0700

    all the things

--- web.js
+++ web.js
@@ -1,9 +1,9 @@
-var express = require('express');
-
-var app = express.createServer(express.logger());
+var express = require("express");
+var app = express();
+app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('yay');
+  response.send('Hello World!');
 });
 
 var port = process.env.PORT || 5000;
